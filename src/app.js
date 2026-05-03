3d525a3709c15a81357053eadd9b7953180cf077
Mind Gym Tanay <tanayagrawal0302@gmail.com>
2026-05-03 17:56:38 +0530

    commit multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,45 +2,68 @@ const express=require('express');
 
 const app=express();
 
-app.use("/user",(req,res)=>{
-    res.send("HAHAHAHAHAHA");
-})
 
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Tanay", lastName:"Agrawal"});
+app.use("/user",[(req,res,next)=>{
 
-})
-
-app.post("/user",(req,res)=>{
-    res.send("Data successfully saved to the database");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("Deleted Successfully");
-})
-
-app.use("/test",(req,res)=>{
-    res.send("Hello from the user!");
-
-})
-
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello Hello");
-
-})
-
+    console.log("Handling the route user");
+    next();
+},
+(req,res,next)=>{
+    console.log("Handling the route user2");
+    next();
+}],
+(req,res,next)=>{
+    console.log("Handling the route user3");
+    next();
+    
+},
+(req,res,next)=>{
+    console.log("Handling the route user 4");
+    next();
+  
+},
+(req,res,next)=>{
+    console.log("Handling the route user 5");
+    res.send("Response5 generated!!")
+  
+}
 
 
-app.use("/hello/2",(req,res)=>{
-    res.send("aabra ka dabra");
-})
-
-
-
-
-
+);
 
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000");
 });
+
+
+// app.get("/user/:userId/:name/:password",(req,res)=>{
+//     console.log(req.params);
+//     res.send({firstName:"Tanay", lastName:"Agrawal"});
+
+// })
+
+// app.post("/user",(req,res)=>{
+//     res.send("Data successfully saved to the database");
+// })
+
+// app.delete("/user",(req,res)=>{
+//     res.send("Deleted Successfully");
+// })
+
+// app.use("/test",(req,res)=>{
+//     res.send("Hello from the user!");
+
+// })
+
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello Hello Hello");
+
+// })
+
+
+// app.use("/hello/2",(req,res)=>{
+//     res.send("aabra ka dabra");
+// })
+
+
 
